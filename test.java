607ecf633de1d67252450c58a41391b9ba4020ae
6830613005-ship-it <6830613005@psu.ac.th>
2025-09-21 23:37:02 +0700

    Odd

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        int age = 3; 
+    if (age%2 == 0) {
+        System.out.print(age +" is Even number");
+    }
+    else {
+        System.out.println(age + "is Odd number");
+    }
+    }
+    
+}
